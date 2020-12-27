6f4badc1e8e7bdbe55145a413e0c0b5fb2b48908
Gareth Francis <gfrancis.dev@gmail.com>
2020-12-27 09:40:00 +0000

    Make binary executable

--- IPALinux/publish-linux-x64.sh
+++ IPALinux/publish-linux-x64.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 buildDir=$(dirname $0)
 dotnet publish /p:PublishProfile=${buildDir}/Properties/PublishProfiles/Linux64-bin.pubxml
+chmod +x bin/Release/net5.0/publish-linux64-bin/IPA-Minimal
 #dotnet publish /p:PublishProfile=${buildDir}/Properties/PublishProfiles/Linux64-lib.pubxml
 
